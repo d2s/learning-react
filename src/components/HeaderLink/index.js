9711dcd57374006f018186f87c503a977ad7f0b5
Daniel Schildt <daniel.schildt@gmail.com>
2017-03-29 17:10:09 +0300

    style(css) add code comments
    - Add code comments to clarify what the elements are.

--- src/components/HeaderLink/index.js
+++ src/components/HeaderLink/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import classNames from 'classnames'
 
+/**
+ * CSS class names
+ */
 const cx = classNames('link dim gray f6 f5-ns dib mr3')
 // const cx = classNames('link dim gray f6 f5-ns dib mr3', {
 //   'black b': isActive
