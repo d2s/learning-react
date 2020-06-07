7d3d1601d0cff138a45561e22682b30510a7e4c2
Daniel Schildt <daniel@autiomaa.org>
2020-06-07 14:24:37 +0300

    fix: ignore ESLint warning
    - Ignore ESLint warning

--- src/components/HeaderLink/index.js
+++ src/components/HeaderLink/index.js
@@ -10,6 +10,7 @@ const cx = classNames('link dim gray f6 f5-ns dib mr3')
 // })
 
 export default (props) => (
+  // eslint-disable-next-line
   <a className={cx} title={props.name} href="#">
     {props.name}
   </a>
